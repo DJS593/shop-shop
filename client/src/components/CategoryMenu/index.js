3a92d1c75755ea5dc48f4f052cecc0831b90dbc3
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-08-06 18:13:40 -0700

    cart now showing in indexedDB

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,34 +1,44 @@
 import React, { useEffect } from "react";
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY, UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
 import { useStoreContext } from "../../utils/GlobalState";
+import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  
   const [state, dispatch] = useStoreContext();
+  const { categories } = state;
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+  // const categories = categoryData?.categories || [];
 
-const { categories } = state;
+  useEffect(() => {
+    // if catgeoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
 
-const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
 
-useEffect(() => {
-  // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-  if (categoryData) {
-    // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+  const handleClick = id => {
     dispatch({
-      type: UPDATE_CATEGORIES,
-      categories: categoryData.categories
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
     });
-  }
-}, [categoryData, dispatch]);
-
-const handleClick = id => {
-  dispatch({
-    type: UPDATE_CURRENT_CATEGORY,
-    currentCategory: id
-  });
-};
+  };
 
   return (
     <div>
