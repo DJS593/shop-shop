7fe38a3c6eb1b40aec8d8b29f4edf7a9a8a34b03
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-08-08 22:40:08 -0700

    stripe added but redirect is still not working

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,10 +5,18 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+// class material insisted on this variable being declared directly after the import statements
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
   const [state, dispatch] = useStoreContext();
+
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -20,6 +28,15 @@ const Cart = () => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
+
+  // useEffect for stripe
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -43,6 +60,22 @@ const Cart = () => {
     return sum.toFixed(2);
   }
   
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
+  
+  
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>[close]</div>
@@ -56,7 +89,7 @@ const Cart = () => {
             <strong>Total: ${calculateTotal()}</strong>
             {
               Auth.loggedIn() ?
-                <button>
+                <button onClick={submitCheckout}>
                   Checkout
                 </button>
                 :
